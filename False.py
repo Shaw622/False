9e3bc3c30d569d9fbc032715e105773fbe56122f
Shaw622 <shaw622.dev@gmail.com>
2019-06-24 23:04:19 +0900

    first commit

---
+++ False.py
@@ -0,0 +1,9 @@
+name = ''
+while not name:
+    print('enter your name')
+    name = input()
+print('同伴者は何名ですか？')
+num_of_guests = int(input())
+if num_of_guests:
+    print('同伴者用の場所があるか確認してください')
+print('受け付けました')
